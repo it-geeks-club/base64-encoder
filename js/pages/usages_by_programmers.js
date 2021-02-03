d8bc39ec295308f82a2defff69f4ccff888bc9f1
Svitlana <r.svetulik@gmail.com>
2021-02-03 04:00:51 +0200

    Refactoring

--- js/pages/usages_by_programmers.js
+++ js/pages/usages_by_programmers.js
@@ -1,4 +1,4 @@
-$(function() {
+$(() => {
     'use strict';
 
     $('#select-pl')
@@ -11,38 +11,8 @@ $(function() {
             dropdownParent: 'body'
         })
         .on('change', function() {
-            var related_table = $('.code-example[data-rel-pl="' + $(this).val() + '"]');
-            if (related_table.size() && (related_table.css('display') === 'none')) {
-                $('.code-example').hide();
-                related_table.show();
-            }
-        });
-
-    $('.code-example th:first-child')
-        .on('mouseenter', function() {
-            $(this)
-                .closest('table')
-                .find('tr > td:first-child')
-                .addClass('on-hover');
-        })
-        .on('mouseleave', function() {
-            $(this)
-                .closest('table')
-                .find('tr > td:first-child')
-                .removeClass('on-hover');
-        });
-
-    $('.code-example th:last-child')
-        .on('mouseenter', function() {
-            $(this)
-                .closest('table')
-                .find('tr > td:last-child')
-                .addClass('on-hover');
-        })
-        .on('mouseleave', function() {
-            $(this)
-                .closest('table')
-                .find('tr > td:last-child')
-                .removeClass('on-hover');
+            $('.code-example').hide();
+            const langName = $(this).val();
+            $(`.code-example[data-rel-pl="${langName}"]`).show();
         });
 });
